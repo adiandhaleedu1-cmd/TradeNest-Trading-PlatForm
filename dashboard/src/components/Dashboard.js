c5aa4fa1b10e69e1431a22603cb3f301bc7092b5
Adi Andhale <adiandhaleedu1@gmail.com>
2026-07-22 14:12:59 +0530

    add raw data file, setup dashboard and make dynamic, holding and position are updating, watchlist is created

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -10,6 +10,8 @@ import Positions from "./Positions";
 import Summary from "./Summary";
 import WatchList from "./WatchList";
 
+import { GeneralContextProvider } from "./GeneralContext";
+
 const Dashboard = () => {
   return (
     <div className="dashboard-container">
